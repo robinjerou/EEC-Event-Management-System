bf0aea0dfcab4d5be7b39adbbf59a35d020c1713
robinjerou <145632932+robinjerou@users.noreply.github.com>
2026-09-12 15:04:19 +0800

    Add files
    github website

---
+++ homescript.js
@@ -0,0 +1,18 @@
+var swiper = new Swiper(".gallery-slider", {
+   loop:true,
+   effect: "coverflow",
+   slidesPerView: "auto",
+   centeredSlides: true,
+   grabCursor: true,
+   coverflowEffect: {
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2,
+      slideShadows: true,
+   },
+   pagination: {
+      el: ".swiper-pagination",
+    },
+});
+
